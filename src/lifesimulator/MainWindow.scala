de00a009efa435341b369d58327fb4a6bc743684
= <=>
2012-04-08 16:21:35 +0200

    Stop timer when closing the main window.

--- src/lifesimulator/MainWindow.scala
+++ src/lifesimulator/MainWindow.scala
@@ -2,10 +2,32 @@ package lifesimulator
 
 import java.awt.{Dimension, BorderLayout}
 import javax.swing._
+import java.awt.event.{WindowEvent, WindowListener}
 
 class MainWindow extends JFrame {
   setTitle("Life Simulator")
   setLayout(new BorderLayout)
+
+  addWindowListener(new WindowListener {
+    def windowClosed(e: WindowEvent) {
+      view.model match {
+        case Some(m) => m.timer.stop()
+        case None =>
+      }
+    }
+
+    def windowDeactivated(e: WindowEvent) {}
+
+    def windowDeiconified(e: WindowEvent) {}
+
+    def windowClosing(e: WindowEvent) {}
+
+    def windowActivated(e: WindowEvent) {}
+
+    def windowIconified(e: WindowEvent) {}
+
+    def windowOpened(e: WindowEvent) {}
+  })
 
   val view = new View
   add(view, BorderLayout.CENTER)
